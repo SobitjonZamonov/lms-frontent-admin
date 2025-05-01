7aa5a82109dc8046e4cb0072e12b5c1edd2bd47f
SobitjonZamonov <sobitjonzamonov@gmail.com>
2025-05-01 16:15:32 +0500

    fixed some bugs

--- src/config/axios-instance.ts
+++ src/config/axios-instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookie from "js-cookie";
 
 export const instance = axios.create({
-  baseURL: 'http://13.233.2.40:4000/api/v1',
+  baseURL: 'http://13.201.9.21:4000/api/v1',
 });
 
 instance.interceptors.request.use((config) => {
